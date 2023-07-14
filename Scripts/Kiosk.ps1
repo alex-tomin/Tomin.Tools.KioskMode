a99daf17c3abc88a5b63b4be641ac740b05a7aae
Oleksandr Tomin <aleto@microsoft.com>
2023-07-14 16:02:15 -0700

    Make Move Window just move window

--- Scripts/Kiosk.ps1
+++ Scripts/Kiosk.ps1
@@ -1,57 +1,22 @@
-﻿#------------------------------------
-#     Functions Definition 
-#   Do NOT TOUCH this section!  
-#------------------------------------
-param
-( 
-  [Parameter(Mandatory)]
-  [string]
-  $Arguments,
+﻿######################################################
+##########            Kiosk Mode          ############
+######################################################
+#
+# Runs edge in full-screen mode on predefined screens
+# ----------------------------------------------------
 
-  [Parameter(Mandatory)]
-  [Int]
-  $MonitorNum,
+Set-Location $PSScriptRoot
 
-  [string]
-  $Program = 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe',
+# Kill all running instances
+# &taskkill /im msedge* /F
 
-  [Int]
-  $StartDelay = 1
-)
+start msedge "--new-window https://eng.ms/" -WindowStyle maximized
+. .\Move-Window.ps1 -ProcessName msedge -MonitorNum 3
 
-Add-Type -assembly System.Windows.Forms
-Add-Type -Name WinAPIHelpers -Namespace WinApi -MemberDefinition '
-    [DllImport("user32.dll")]
-    public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int W, int H); 
-    
-    [DllImport("user32.dll")]
-	public static extern bool SetForegroundWindow(IntPtr hWnd);
-    
-    [DllImport("user32.dll")]
-	[return: MarshalAs(UnmanagedType.Bool)]
-	public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);'
+start msedge "--new-window https://contoso.com"
+. .\Move-Window.ps1 -ProcessName msedge -MonitorNum 2
+# move first before opening another tab, as the windows needs to be active,
+start msedge "https://stackoverflow.com"
 
-$WinAPI = [WinApi.WinAPIHelpers]
-$Screen = [System.Windows.Forms.Screen]
-$SendKeys = [System.Windows.Forms.SendKeys]
-
-
-
-Write-Output "starting $Program with $Arguments , monitor: $MonitorNum"
-Start-Process -FilePath $Program $Arguments   # note chrome main window will be destroyed, so cannot use handle returned from start-process command.
-
-Start-Sleep -Seconds $StartDelay
-$window = (Get-Process -Name msedge | where {$_.MainWindowHandle -ne ([IntPtr]::Zero)} | select -First 1).MainWindowHandle
-
-$monitor =  $MonitorNum-1;
-$left = $Screen::AllScreens[$monitor].WorkingArea.Left
-$top = $Screen::AllScreens[$monitor].WorkingArea.Top
-
-$WinAPI::ShowWindow($window, 9) # 9 == Restore
-$WinAPI::MoveWindow($window, $left, $top, 1000, 800)
-
-$WinAPI::SetForegroundWindow($window);
-$SendKeys::SendWait("{F11}");
-$SendKeys::Flush();
-
-Start-Sleep -Seconds $StartDelay
+start msedge "--new-window https://contoso.com"
+. .\Move-Window.ps1 -ProcessName -MonitorNum 1
---
+++ Scripts/Move-Window.ps1
@@ -0,0 +1,51 @@
+﻿#------------------------------------
+#     Functions Definition 
+#   Do NOT TOUCH this section!  
+#------------------------------------
+param
+( 
+  [Parameter(Mandatory)]
+  [string]
+  $ProcessName,
+
+  [Parameter(Mandatory)]
+  [Int]
+  $MonitorNum,
+
+  [Int]
+  $StartDelay = 1
+)
+
+Add-Type -assembly System.Windows.Forms
+Add-Type -Name WinAPIHelpers -Namespace WinApi -MemberDefinition '
+    [DllImport("user32.dll")]
+    public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int W, int H); 
+    
+    [DllImport("user32.dll")]
+	  public static extern bool SetForegroundWindow(IntPtr hWnd);
+    
+    [DllImport("user32.dll")]
+	  [return: MarshalAs(UnmanagedType.Bool)]
+	  public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);'
+
+$WinAPI = [WinApi.WinAPIHelpers]
+$Screen = [System.Windows.Forms.Screen]
+$SendKeys = [System.Windows.Forms.SendKeys]
+
+Start-Sleep -Seconds $StartDelay
+$window = (Get-Process -Name $ProcessName | where {$_.MainWindowHandle -ne ([IntPtr]::Zero)} | select -First 1).MainWindowHandle
+
+Write-Output "Moving $ProcessName to monitor: $MonitorNum"
+
+$monitor =  $MonitorNum-1;
+$left = $Screen::AllScreens[$monitor].WorkingArea.Left
+$top = $Screen::AllScreens[$monitor].WorkingArea.Top
+
+$WinAPI::ShowWindow($window, 9) # 9 == Restore
+$WinAPI::MoveWindow($window, $left, $top, 1000, 800)
+
+$WinAPI::SetForegroundWindow($window);
+$SendKeys::SendWait("{F11}");
+$SendKeys::Flush();
+
+Start-Sleep -Seconds $StartDelay
